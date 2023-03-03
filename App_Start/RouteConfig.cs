81c780c3b1870d18e64c9175302168e1d8d33c36
Claudiarf28 <94508973+Claudiarf28@users.noreply.github.com>
2023-03-03 03:03:29 -0600

    compras

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace compras
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "compras/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
