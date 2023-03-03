e631ec69fc807de290647ed38f3bccd487d1a671
Manuel Alvarado <manuelarturo120@gmail.com>
2023-03-02 21:38:20 -0600

    get de guias

---
+++ BD/GuideDAO.cs
@@ -0,0 +1,43 @@
+﻿using compras.BD.Entities;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace compras.BD
+{
+    public class GuideDAO
+    {
+        private readonly string con = ConfigurationManager.ConnectionStrings["Comedor"].ConnectionString;
+
+        public List<GuidesEntity> GetGuides()
+        {
+            try
+            {
+                List<GuidesEntity> guides = new List<GuidesEntity>();
+
+                using (var db = new SqlConnection(con))
+                {
+                    db.Open();
+                    guides = db.Query<GuidesEntity>("SELECT Id_Guide,numEmpployed,Name,conpany,destination," +
+                        "description,size,kg,ledgerAccount,costCenter,guide FROM Guides", commandType: CommandType.Text).ToList();
+                    db.Close();
+                    return guides;
+                }
+
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ Service/GuideService.cs
@@ -0,0 +1,30 @@
+﻿using compras.BD;
+using compras.BD.Entities;
+using compras.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace compras.Service
+{
+    public class GuideService
+    {
+        public List<GuidesRS> getGuides()
+        {
+            try
+            {
+                GuideDAO dao = new GuideDAO();
+                return  assemblerGuides (dao.GetGuides());
+            } catch (Exception e)
+            {
+                throw e;
+            }
+        } 
+        private List<GuidesRS> assemblerGuides(List<GuidesEntity> guides)
+        {
+            return guides.ConvertAll(x => new GuidesRS(x.numEmpployed, x.Name, x.conpany, x.destination, x.description,
+         x.size, x.kg, x.ledgerAccount, x.costCenter, x.guide));
+        }
+    }
+}
