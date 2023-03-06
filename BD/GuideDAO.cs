364bee7a6f9ed7a2b006dde8454d4225c188e5f9
Claudiarf28 <94508973+Claudiarf28@users.noreply.github.com>
2023-03-06 02:39:12 -0600

    Se mofican rutas del menu
    servicios de menu

--- BD/GuideDAO.cs
+++ BD/GuideDAO.cs
@@ -23,8 +23,59 @@ namespace compras.BD
                 using (var db = new SqlConnection(con))
                 {
                     db.Open();
-                    guides = db.Query<GuidesEntity>("SELECT Id_Guide,numEmpployed,Name,conpany,destination," +
-                        "description,size,kg,ledgerAccount,costCenter,guide FROM Guides", commandType: CommandType.Text).ToList();
+                    guides = db.Query<GuidesEntity>(" SELECT g.Id_Guide, g.numEmpployed, g.Name, g.conpany, g.destination, g.description, g.size, g.kg, g.ledgerAccount, " +
+                        "g.costCenter, g.guide, cg.type as guideType, cg.price FROM Guides g INNER JOIN Cat_Guide cg ON g.guide = cg.Id_Guide_Type", commandType: CommandType.Text).ToList();
+                    db.Close();
+                    return guides;
+                }
+
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<CatGuidesEntity> GetIdGuides()
+        {
+            try
+            {
+                List<CatGuidesEntity> guides = new List<CatGuidesEntity>();
+
+                using (var db = new SqlConnection(con))
+                {
+                    db.Open();
+                    guides = db.Query<CatGuidesEntity>(" SELECT  Id_Guide_Type, price, type  FROM Cat_Guide", commandType: CommandType.Text).ToList();
+                    db.Close();
+                    return guides;
+                }
+
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public int Getcount (int guide)
+        {
+            try
+            {
+                int guides = 0;
+
+                using (var db = new SqlConnection(con))
+                {
+                    db.Open();
+                    guides = db.Query<int>(" select count(guide) from Guides where guide=@guide ",
+                        new { guide = guide}).FirstOrDefault();
                     db.Close();
                     return guides;
                 }
