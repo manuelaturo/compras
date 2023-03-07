4a68c6b1854144891b516842f9bbd1c102fb1c89
Manuel Alvarado <manuelarturo120@gmail.com>
2023-03-07 17:01:44 -0600

    merge de guias

--- Service/GuideService.cs
+++ Service/GuideService.cs
@@ -44,8 +44,48 @@ namespace compras.Service
             {
                 throw e;
             }
-        } 
-        
+        }
+
+        public GuideResult getGuides(string dateinit, string dateend)
+        {
+            try
+            {
+
+                var dateInit = DateTime.ParseExact(dateinit, "dd-MM-yyyy", System.Globalization.CultureInfo.CurrentUICulture.DateTimeFormat);
+                var dateEnd = DateTime.ParseExact(dateend, "dd-MM-yyyy", System.Globalization.CultureInfo.CurrentUICulture.DateTimeFormat);
+
+                GuideResult response = new GuideResult();
+                GuideDAO dao = new GuideDAO();
+                var result = dao.GetIdGuides();
+                decimal costTotal = 0;
+
+                List<guidesTotal> guidesTotals = new List<guidesTotal>();
+
+                foreach (var item in result)
+                {
+                    guidesTotal guidesTotal = new guidesTotal();
+                    var idGuide = item.Id_Guide_Type;
+                    var count = dao.Getcount(idGuide);
+                    var total = count * item.price;
+                    guidesTotal.guide = item.type;
+                    guidesTotal.price = item.price;
+                    guidesTotal.count = count;
+                    costTotal = costTotal + total;
+                    guidesTotals.Add(guidesTotal);
+                }
+
+                response.guidesRs = assemblerGuides(dao.GetGuides(dateInit, dateEnd));
+                response.totalCost = costTotal;
+                response.guidesTotals = guidesTotals;
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
         private List<GuidesRS> assemblerGuides(List<GuidesEntity> guides)
         {
             return guides.ConvertAll(x => new GuidesRS(x.numEmpployed, x.Name, x.conpany, x.destination, x.description,
